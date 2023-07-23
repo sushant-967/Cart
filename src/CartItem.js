874c002e6710d8c6b49921606e23c81c916922bb
sushant-967 <sushantagrawal967@gmail.com>
2023-07-23 19:05:09 +0530

    modify the state (increasing and decreasing quantity on click) using setstate

--- src/CartItem.js
+++ src/CartItem.js
@@ -11,7 +11,20 @@ class CartItem extends React.Component{
         }
     }
     increaseQuantity=()=>{
-        console.log('this',this);
+        
+        console.log('this',this.state);
+        this.setState({
+            qty: this.state.qty+1
+        });
+    }
+    decreaseQuantity=()=>{
+        const {qty}=this.state;
+        if(qty==0)
+        return;
+        this.setState({
+            qty: this.state.qty-1
+        });
+        
     }
     render(){
         const{price,title,qty}=this.state;
@@ -29,7 +42,8 @@ class CartItem extends React.Component{
                         {/*Buttons*/}
                         <img alt="increase" className="action-icons"src="https://cdn-icons-png.flaticon.com/128/992/992651.png"
                         onClick={this.increaseQuantity}/>
-                        <img alt="decrease" className="action-icons"src="https://cdn-icons-png.flaticon.com/128/992/992683.png"/>
+                        <img alt="decrease" className="action-icons"src="https://cdn-icons-png.flaticon.com/128/992/992683.png"
+                        onClick={this.decreaseQuantity}/>
                         <img alt="delete" className="action-icons"src="https://cdn-icons-png.flaticon.com/128/3405/3405244.png"/>
                         
                     </div>
