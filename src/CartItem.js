23d9a7cb1bdba5552667c988238f7b08fff7f63c
sushant-967 <sushantagrawal967@gmail.com>
2023-07-29 17:33:46 +0530

    add props to cartitem to pass properties from parent to child that is from cart to cartitem

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,15 +1,6 @@
 import React from 'react';
 class CartItem extends React.Component{
     
-    constructor(){
-        super();
-        this.state={
-            price:999,
-            title:'Phone',
-            qty:1,
-            img:''
-        }
-    }
     increaseQuantity=()=>{
         
         console.log('this',this.state);
@@ -27,7 +18,8 @@ class CartItem extends React.Component{
         
     }
     render(){
-        const{price,title,qty}=this.state;
+        console.log('this.props',this.props);
+        const{price,title,qty}=this.props.product;
         return (
             <div className="cart-item">
                 <div className="left-block">
